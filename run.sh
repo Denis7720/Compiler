b588d0dd95acf299ef998269755fd831c4bfa4bc
Denis7720 <36222436+Denis7720@users.noreply.github.com>
2020-06-20 12:10:52 +0700

    Add files via upload

---
+++ run.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+python3 compiler.py /dump-tokens hello_world.py
+python3 compiler.py /dump-tokens minmax.py
+python3 compiler.py /dump-tokens nod.py
+python3 compiler.py /dump-tokens strinstr.py
